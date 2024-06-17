d57a1a86862c9975853f88411679e9d8c5e24b05
AirenMarie <astringer92@gmail.com>
2024-06-17 19:16:08 -0400

    Add if and else statements to buyHealth

--- rpg_demo.js
+++ rpg_demo.js
@@ -73,7 +73,16 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 
 function buyWeapon() {}
 
