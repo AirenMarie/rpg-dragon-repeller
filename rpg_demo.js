20d7f91768668a5530cc77f7b58fe1c2435d021d
AirenMarie <astringer92@gmail.com>
2024-06-18 20:37:09 -0400

    Place monsterHealth variable in attack within if block

--- rpg_demo.js
+++ rpg_demo.js
@@ -194,8 +194,10 @@ function attack() {
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -=
-    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
